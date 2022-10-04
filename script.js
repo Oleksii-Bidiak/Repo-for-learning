4961c970c30d11f8e8605c7c215a446952eb5900
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2022-10-04 14:55:52 +0300

    Complete the second taks

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log('Задача №1')
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+const numberOfFilms = +prompt('Скольки фільмів ви вже переглянули?', '')
 console.log(numberOfFilms)
 
-console.log('Задача №2')
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -11,9 +9,33 @@ const personalMovieDB = {
    privat: false
 }
 
-console.log('Задача №3')
 const movies = {}
-const movie = prompt('Один из последних просмотренных фильмов?', '')
-const rating = +prompt('На сколько оцените его?', '')
-personalMovieDB.movies[movie] = rating
+
+for (let i = 0; i < 2; i++) {
+   const movie = prompt('Один з останніх переглянутих фільмів?', ''),
+      rating = prompt('На скільки ви його оцінюєте?', '')
+
+   if (movie !== null && rating !== null && movie !== '', rating !== '', movie?.length < 50) {
+      personalMovieDB.movies[movie] = +rating
+   } else {
+      --i
+   }
+}
+
+if (personalMovieDB.count < 10) {
+   console.log('Переглянуто доволі мало фільмів')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('Ви класичний глядач')
+} else if (personalMovieDB.count >= 30) {
+   console.log('Ви кіноман')
+} else {
+   console.log('Виникла помилка')
+}
+
+// personalMovieDB.count < 10 ? console.log('Переглянуто доволі мало фільмів')
+//    : personalMovieDB.count >= 10 && personalMovieDB.count < 30 ? console.log('Ви класичний глядач') 
+//    : console.log('Ви кіноман')
+
 console.log(personalMovieDB)
+
+
