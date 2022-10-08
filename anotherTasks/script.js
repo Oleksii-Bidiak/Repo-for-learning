5215a62eb62928f414e1a51cc18fb0f6ccfca6cd
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2022-10-08 09:29:42 +0300

    Complete the 5 task

---
+++ anotherTasks/script.js
@@ -0,0 +1,74 @@
+const personalMovieDB = {
+   count: 0,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+
+   start: function () {
+      numberOfFilms = prompt('Скольки фільмів ви вже переглянули?', '')
+
+      while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+         numberOfFilms = prompt('Скільки фільмів ви вже переглянули?', '')
+      }
+
+      this.count = +numberOfFilms
+   },
+
+   rememberMyFilms: function () {
+      for (let i = 0; i < 2; i++) {
+         const movie = prompt('Один з останніх переглянутих фільмів?', ''),
+            rating = prompt('На скільки ви його оцінюєте?', '')
+
+         if (movie !== null || rating !== null || movie !== '' || rating !== '' || movie?.length < 50) {
+            this.movies[movie] = +rating
+         } else {
+            --i
+         }
+      }
+   },
+
+   detectPersonalLevel: function () {
+      if (this.count < 10) {
+         console.log('Переглянуто доволі мало фільмів')
+      } else if (this.count >= 10 && this.count < 30) {
+         console.log('Ви класичний глядач')
+      } else if (this.count >= 30) {
+         console.log('Ви кіноман')
+      } else {
+         console.log('Виникла помилка')
+      }
+   },
+
+   showMyDB: function () {
+      if (this.privat) {
+         console.log('It\'s a privat DB')
+      } else {
+         console.log(this)
+      }
+   },
+
+   writeYourGenres: function () {
+      for (let i = 0; i < 1; i++) {
+         const genres = prompt(`Введіть ваші улюблені жанри через кому`, '')
+
+         if (genres === null || genres === '') {
+            console.log('Ви ввели некоректні дані або не ввели взагалі')
+            i--
+         } else {
+            this.genres = genres.split(', ').map(item => { return `${item.split('')[0].toUpperCase()}${item.slice(1)}` }).sort()
+         }
+      }
+
+      this.genres.forEach((item, i) => {
+         console.log(`Улюблений жанр ${i + 1} - це ${item}`)
+      })
+   },
+
+   toggleVisibleMyDB: function () {
+      this.privat = !this.privat
+   }
+}
+
+personalMovieDB.writeYourGenres()
+
--- project/js/script.js
+++ project/js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+5) Фильмы должны быть отсортированы по алфавиту */
 
-5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
@@ -28,7 +31,11 @@ movieDB.movies.sort()
 const advs = document.querySelectorAll('.promo__adv img'),
     poster = document.querySelector('.promo__bg'),
     promoGenre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+    movieList = document.querySelectorAll('.promo__interactive-list > li'),
+    form = document.querySelector('.add'),
+    inputForm = form.getElementsByClassName('adding__input'),
+    btnAdd = form.querySelector('button'),
+    checkbox = form.querySelector('[type="checkbox"]');
 
 advs.forEach(item => {
     item.remove()
@@ -40,18 +47,52 @@ poster.style.backgroundImage = "url('img/bg.jpg')"
 
 movieList.innerHTML = ''
 movieDB.movies.forEach((item, i) => {
+    let movie = item
+    if (movie.length > 21) {
+        movie = movie.slice(0, 19) + '...'
+    }
+
     movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${item}
+        <li class="promo__interactive-item">${i + 1} ${movie}
             <div class="delete"></div>
         </li>
     `
 })
 
-// for (let i = 0; i < movieDB.movies.length; i++) {
-//     const item = movieDB.movies[i]
-//     const promoListItem = document.createElement('li')
-//     promoListItem.classList.add('promo__interactive-item')
-//     promoListItem.textContent = item
-//     console.log(promoListItem)
-//     promoList.insertAdjacentHTML('beforebegin', promoListItem)
-// }
+btnAdd.addEventListener('click', setNewFilm)
+
+function setNewFilm(e) {
+    e.preventDefault()
+
+    const fav = checkbox.value
+
+    if (inputForm[0].value !== '') {
+        const newFilm = inputForm[0].value.split(', ').map(item => { return `${item.split('')[0].toUpperCase()}${item.slice(1)}` }).join('')
+        movieDB.movies.push(newFilm)
+        movieDB.movies.sort()
+
+        if (fav) {
+            console.log('Улюблений')
+        }
+
+        movieList.innerHTML = ''
+        movieDB.movies.forEach((item, i) => {
+            let movie = item
+            if (movie.length > 21) {
+                movie = movie.slice(0, 19) + '...'
+            }
+
+            movieList.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${movie}
+                    <div class="delete"></div>
+                </li>
+            `
+        })
+    }
+}
+
+movieList.forEach(element => {
+    element.addEventListener('click', () => {
+        element.remove()
+    })
+});
