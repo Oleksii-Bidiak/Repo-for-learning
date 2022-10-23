c0e6614086187a2a72acf1d86049321edf7ec5b1
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2022-10-23 14:11:22 +0300

    implementation of the timer on the page

--- Food/js/script.js
+++ Food/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+   // Tabs
    const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -36,4 +37,57 @@ document.addEventListener('DOMContentLoaded', () => {
          })
       }
    })
+
+   // Timer
+   const deadline = '2022-10-24'
+
+   const getTimeRemaining = (endtime) => {
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+         minute = Math.floor((t / (1000 * 60)) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+
+      return {
+         'total': t,
+         days,
+         hours,
+         minute,
+         seconds
+      }
+   }
+
+   const getZero = (num) => {
+      if (num < 10) {
+         return `0${num}`
+      }
+      return num
+   }
+
+   const setClock = (selector, endtime) => {
+      const timer = document.querySelector(selector),
+         days = timer.querySelector('#days'),
+         hours = timer.querySelector('#hours'),
+         minutes = timer.querySelector('#minutes'),
+         seconds = timer.querySelector('#seconds');
+
+      const updateClock = () => {
+         const t = getTimeRemaining(endtime)
+
+         days.innerHTML = getZero(t.days)
+         hours.innerHTML = getZero(t.hours)
+         minutes.innerHTML = getZero(t.minute)
+         seconds.innerHTML = getZero(t.seconds)
+
+         if (t.total <= 0) {
+            clearInterval(timeInterval)
+         }
+      }
+
+      const timeInterval = setInterval(updateClock, 1000)
+
+      updateClock()
+   }
+
+   setClock('.timer', deadline)
 })
