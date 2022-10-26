141aa9b1d7e657ee219b660cbbdf4cddca7d4ced
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2022-10-26 16:27:21 +0300

    implementation of dynamic output menuCard

--- Food/js/script.js
+++ Food/js/script.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
    })
 
    // Timer
-   const deadline = '2022-10-24'
+   const deadline = '2022-10-28'
 
    const getTimeRemaining = (endtime) => {
       const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -58,7 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
    }
 
    const getZero = (num) => {
-      if (num < 10) {
+      if (num <= 0) {
+         return 0
+      } else if (num < 10 && num >= 0) {
          return `0${num}`
       }
       return num
@@ -97,16 +99,17 @@ document.addEventListener('DOMContentLoaded', () => {
       modal = document.querySelector('.modal'),
       closeBtn = document.querySelector('[data-close]');
 
-   const closeModal = () => {
+   const openModal = () => {
       modal.classList.toggle('show')
-      document.body.style.overflow = ''
-   }
+      document.body.style.overflow = 'hidden'
+   },
+      closeModal = () => {
+         modal.classList.toggle('show')
+         document.body.style.overflow = ''
+      }
 
    modalBtns.forEach(item => {
-      item.addEventListener('click', () => {
-         modal.classList.toggle('show')
-         document.body.style.overflow = 'hidden'
-      })
+      item.addEventListener('click', openModal)
    })
 
    closeBtn.addEventListener('click', closeModal)
@@ -123,4 +126,69 @@ document.addEventListener('DOMContentLoaded', () => {
          closeModal()
       }
    })
+
+   // Cards
+
+   class menuCard {
+      constructor(src, alt, title, description, price, parentSelector) {
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.description = description;
+         this.price = price;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 41;
+         this.changeToUAH()
+         this.render()
+      }
+
+      changeToUAH() {
+         this.price *= +this.transfer
+      }
+
+      render() {
+         const element = document.createElement('div')
+         element.innerHTML = `
+            <div class="menu__item">
+               <img src=${this.src} alt=${this.alt}>
+               <h3 class="menu__item-subtitle">${this.title}</h3>
+               <div class="menu__item-descr">${this.description}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+               </div>
+            </div>
+         `
+
+         this.parent.append(element)
+      }
+   }
+
+   new menuCard(
+      'img/tabs/vegy.jpg',
+      'vegy',
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      '9',
+      '.menu .container'
+   )
+
+   new menuCard(
+      'img/tabs/elite.jpg',
+      'elite',
+      'Меню “Премиум”',
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      '9',
+      '.menu .container'
+   )
+
+   new menuCard(
+      'img/tabs/post.jpg',
+      'post',
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      '9',
+      '.menu .container'
+   )
 })
